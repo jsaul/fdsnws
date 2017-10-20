8ff764e48e59bb7d674cb8e9c8b7ac9519c8ceae
Joachim Saul <saul@gfz-potsdam.de>
2017-10-20 12:25:30 +0200

    cosmetics

--- fdsnws/paz.py
+++ fdsnws/paz.py
@@ -1,5 +1,6 @@
 import sys
 from obspy.core.inventory.response import PolesZerosResponseStage
+from obspy.core import AttribDict
 
 pz_header_template = """* **********************************
 * NETWORK   (KNETWK): %(net)s
@@ -40,7 +41,11 @@ def rectify_unit(unit):
     unit = unit.upper()
     if unit in [ "M/S/S", "M/S^2" ]:
         unit = "M/S**2"
-    assert unit in valid_units
+    try:
+        assert unit in valid_units
+    except:
+        print unit
+        raise
     return unit
 
 def nslc(pz):
@@ -55,15 +60,15 @@ def obspy_nsc2sacpz(net, sta, cha, input_unit=None):
     if input_unit is not None:
         input_unit = rectify_unit(input_unit)
 
-    pz = {
-        "net" : net.code,
-        "sta" : sta.code,
-        "loc" : cha.location_code,
-        "cha" : cha.code }
+    pz = AttribDict(
+        net = net.code,
+        sta = sta.code,
+        loc = cha.location_code,
+        cha = cha.code )
 
 
-    pz["depth"] = cha.depth
-    pz["start_date"] = str(cha.start_date)[:19]
+    pz.depth = cha.depth
+    pz.start_date = str(cha.start_date)[:19]
 
 #   if cha.end_date is not None:
 #       if cha.end_date.timestamp > 1<<31:
@@ -75,39 +80,44 @@ def obspy_nsc2sacpz(net, sta, cha, input_unit=None):
 #   pz["created"] = str(inventory.created)[:19]
 
 
-    pz["description"] = cha.description
-    pz["dip"] = cha.dip
-    pz["azimuth"] = cha.azimuth
-    pz["fsamp"] = cha.sample_rate
-    pz["lat"] = cha.latitude
-    pz["lon"] = cha.longitude
-    pz["ele"] = cha.elevation
+    pz.description = cha.description
+    pz.dip = cha.dip
+    pz.azimuth = cha.azimuth
+    pz.fsamp = cha.sample_rate
+    pz.lat = cha.latitude
+    pz.lon = cha.longitude
+    pz.ele = cha.elevation
 
     pz_stage = None
     for stage in cha.response.response_stages:
         if type(stage) == PolesZerosResponseStage:
+            if not stage.pz_transfer_function_type.upper().startswith("LAPLACE"):
+                continue
             if len(stage.poles)>0 or len(stage.zeros)>0:
-                if pz_stage is not None:
-                    sys.stderr.write("%s: more than one PZ stage found\n" % nslc(pz))
-                pz_stage = stage
-    pz["a0"]  = pz_stage.normalization_factor
+#               if pz_stage is None:
+                    pz_stage = stage
+#               else:
+#                   sys.stderr.write("%s: more than one PZ stage found - skipping this one\n" % nslc(pz))
+    if not pz_stage:
+        return
+    pz.a0  = pz_stage.normalization_factor
 
     if not cha.sensor.manufacturer and not cha.sensor.model:
         # sloppy XML produced by the IRIS fdsnws
-        pz["sensor_type"] = cha.sensor.type
+        pz.sensor_type = cha.sensor.type
     else:
         # properly populated XML
-        pz["sensor_type"] = "%s %s" % (cha.sensor.manufacturer, cha.sensor.model)
-    pz["sni"] = cha.response.instrument_sensitivity.input_units
-    pz["sno"] = cha.response.instrument_sensitivity.output_units
-    if not pz["sno"] or pz["sno"] == "None":
+        pz.sensor_type = "%s %s" % (cha.sensor.manufacturer, cha.sensor.model)
+    pz.sni = cha.response.instrument_sensitivity.input_units.upper()
+    pz.sno = cha.response.instrument_sensitivity.output_units.upper()
+    if not pz.sno or pz.sno == "None":
         sys.stderr.write("Warning: %s: setting empty OutputUnits to 'COUNTS'\n" % nslc(pz))
-        pz["sno"] = "COUNTS"
-    pz["sensitivity_value"] = cha.response.instrument_sensitivity.value
-    pz["sensitivity_frequency"] = cha.response.instrument_sensitivity.frequency
-    pz["sgn"] = pz_stage.stage_gain
-    pz["sgf"] = pz_stage.stage_gain_frequency
-    pz["inu"] = "%s / %s" % (pz_stage.output_units, pz_stage.input_units)
+        pz.sno = "COUNTS"
+    pz.sensitivity_value = cha.response.instrument_sensitivity.value
+    pz.sensitivity_frequency = cha.response.instrument_sensitivity.frequency
+    pz.sgn = pz_stage.stage_gain
+    pz.sgf = pz_stage.stage_gain_frequency
+    pz.inu = "%s / %s" % (pz_stage.output_units, pz_stage.input_units)
     if pz_stage.pz_transfer_function_type == "LAPLACE (RADIANS/SECOND)":
         factor = 1
     elif pz_stage.pz_transfer_function_type == "LAPLACE (HERTZ)":
@@ -125,9 +135,9 @@ def obspy_nsc2sacpz(net, sta, cha, input_unit=None):
                 pz_stage.zeros.extend(dnz*[0.])
             else:
                 raise NotImplementedError, "removal of zeros not implemented"
-        pz["snj"] = input_unit
+        pz.snj = input_unit
     else:
-        pz["snj"] = pz["sni"]
+        pz.snj = pz.sni
 
     zeros = []
     poles = []
@@ -135,18 +145,20 @@ def obspy_nsc2sacpz(net, sta, cha, input_unit=None):
     sacpz = "ZEROS   %d\n" % len(pz_stage.zeros)
     for zero in pz_stage.zeros:
         zero = complex(zero)*factor
-        pz["a0"] /= factor
+        pz.a0 /= factor
         sacpz += "        %+.6e %+.6e\n" % (zero.real, zero.imag)
         zeros.append(zero)
     sacpz += "POLES   %d\n" % len(pz_stage.poles)
     for pole in pz_stage.poles:
         pole = complex(pole)*factor
-        pz["a0"] *= factor
+        pz.a0 *= factor
         sacpz += "        %+.6e %+.6e\n" % (pole.real, pole.imag)
         poles.append(pole)
     sacpz += "CONSTANT %.6e\n" % (pz["a0"]*pz["sensitivity_value"])
     sacpz  = pz_header_template % pz + sacpz
-    pz["sacpz"] = sacpz
+    pz.sacpz = sacpz
+    pz.poles = poles
+    pz.zeros = zeros
     return pz
 
 
